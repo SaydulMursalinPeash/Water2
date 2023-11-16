f817c37804ae2d78cc244a7c5fe48efb80205246
Saydul Mursalin Peash <saydulpeash428@gmail.com>
2023-11-17 05:33:46 +0600

    rg

---
+++ monitor.py
@@ -0,0 +1,37 @@
+import asyncio
+import websockets
+import flet as ft
+import json
+import asyncio
+import websockets
+'''
+async def main():
+    async with websockets.connect("ws://127.0.0.1:8000/ws/sensor/some_sensor_name/") as websocket:
+        while True:
+            try:
+                data = await websocket.recv()
+                print(f"Received message: {data}")
+            except websockets.exceptions.ConnectionClosedError:
+                print("Connection closed")
+                break
+'''
+
+async def flet_app(page:ft.Page):
+    text=ft.Text()
+    await page.add_async(text)
+    async with websockets.connect("ws://127.0.0.1:8000/ws/sensor/some_sensor_name/") as websocket:
+        while True:
+            try:
+                data = await websocket.recv()
+                data=json.loads(data)
+                print(data['temp'])
+                text.value=data['temp']
+                await page.update_async()
+            except websockets.exceptions.ConnectionClosedError:
+                print("Connection closed")
+                break
+    
+ft.app(target=flet_app)
+
+
+
