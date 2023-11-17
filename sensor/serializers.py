f41ac6c4ef9b48b6dcbe412ec4844dc7201e487e
Saydul Mursalin Peash <saydulpeash428@gmail.com>
2023-11-18 04:07:06 +0600

    shsrthr

--- sensor/serializers.py
+++ sensor/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import SenosrInstance
+from .models import SenosrInstance
 class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model=SenosrInstance
